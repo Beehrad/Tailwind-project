154c09a683dcff78330567ac16de6eab08b29d71
Beehrad <beehrad@yahoo.com>
2023-05-12 15:06:00 +0200

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {
-      
+      colors:{
+        "back" : "#292d33",
+        "khakestari" : "#d9d9d9 "
+      }
     },
   },
   plugins: [],
